aee210f8274af7aad32d42ff37f8b4e2b93c13b7
puru13794 <puru13794@gmail.com>
2023-02-12 15:31:43 +0530

    added rendering conponents

--- src/App.js
+++ src/App.js
@@ -41,9 +41,7 @@ const App = () => {
     <BillForm newbillform={finalprops}/>
     <Card className="expenses">
       <FilterComponent selected={year} onYearchange={YearHandler}/>
-      <FirstComponent title= {expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}  ></FirstComponent>
-      <FirstComponent title= {expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></FirstComponent>
-      <FirstComponent title= {expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></FirstComponent>
+      {expenses.map(expense => <FirstComponent title= {expense.title} amount={expense.amount} date={expense.date} />)}
     </Card>
     </div>
   );
--- src/CustomComponents/FirstComponent.js
+++ src/CustomComponents/FirstComponent.js
@@ -6,15 +6,11 @@ import Card from './Card'
 
 function FirstComponent(props){
   const [title, SetTitle] = useState(props.title);
-    const eventHandler = () => {
-      SetTitle("Done!!!");
-    }
     return (<Card className='expense-item'>
             <DateComponent date={props.date}/>
             <div className='expense-item__description'>
             <h2>{title}</h2>
             <div className='expense-item__price'>{props.amount}</div>
-            <button onClick={eventHandler}>Title Change</button>
             </div>
           </Card>)
 }
